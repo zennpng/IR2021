73a4da766a08602ea7c163326f4788b971650607
zenn <zennpng@gmail.com>
2021-06-26 20:54:18 +0800

    extended stopwords

--- query_preprocessing.py
+++ query_preprocessing.py
@@ -2,12 +2,13 @@ import nltk
 
 def process_query(query):
     stopwords = nltk.corpus.stopwords.words("english")
+    stopwords.extend(['please', 'want', 'like', 'music', 'thanks', 'need', 'require'])  # extend stopwords manually
     query_tokens = nltk.word_tokenize(query)
     query_tokens = [w for w in query_tokens if w.lower() not in stopwords]
     return query_tokens
 
-test_query = "i woke up and choose violence"
-# print(process_query(test_query))
-# ['need', 'workout', 'energy']
+test_query = "i need some workout energy now please thanks"
+print(process_query(test_query))
+# ['workout', 'energy']
 
 
