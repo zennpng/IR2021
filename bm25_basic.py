f80685970a9bb84679a4224ddf6423cc3a0ca1d4
zenn <zennpng@gmail.com>
2021-06-28 16:41:56 +0800

    [clean] -

--- bm25_basic.py
+++ bm25_basic.py
@@ -15,57 +15,57 @@ def query_expansion(query_list):
         expanded_query += expansion
     return(expanded_query)
 
-query = query_expansion(["workout", "energy"])
+# query = query_expansion(["workout", "energy"])
 # print(query)
 # ['exercise', 'workout', 'exercising', 'energy', 'zip', 'vim', 'Energy', 'vigour', 'vigor', 'vitality', 'muscularity', 'get-up-and-go', 'push', 'DOE']
 
 
 ## BM25 (Basic Version)
+def bm25_basic(query, n=5):
+    with open('BM25_index.txt','rb') as handle:
+        indexes = pickle.loads(handle.read())
 
-with open('BM25_index.txt','rb') as handle:
-    indexes = pickle.loads(handle.read())
-#print(indexes["holiday"])
+    def _score(query, doc_id, docs, index, k1=1.5, b=0.75):
+        score = 0.0
+        corpus_size = 28372
+        avg_doc_len = 0 
+        for d in docs:
+            avg_doc_len += len(d)
+        avg_doc_len = avg_doc_len/len(docs)
+        for term in query:
+            if term not in index.keys():
+                continue
+            df = index[term]["doc_freq"] 
+            idf = round(math.log((corpus_size)/(df)),2)
+            tf = 0 
+            for tfset in index[term]["doc_list"]:
+                if tfset[0] == doc_id:
+                    tf = tfset[1]
+            doc_len = len(docs[doc_id-1])
+            score += idf*((k1+1)*tf)/(k1*((1-b)+b*(doc_len/avg_doc_len))+tf)
+        return score
 
-def _score(query, doc_id, docs, index, k1=1.5, b=0.75):
-    score = 0.0
-    corpus_size = 28372
-    avg_doc_len = 0 
-    for d in docs:
-        avg_doc_len += len(d)
-    avg_doc_len = avg_doc_len/len(docs)
-    for term in query:
-        if term not in index.keys():
-            continue
-        df = index[term]["doc_freq"] 
-        idf = round(math.log((corpus_size)/(df)),2)
-        tf = 0 
-        for tfset in index[term]["doc_list"]:
-            if tfset[0] == doc_id:
-                tf = tfset[1]
-        doc_len = len(docs[doc_id-1])
-        score += idf*((k1+1)*tf)/(k1*((1-b)+b*(doc_len/avg_doc_len))+tf)
-    return score
+    music_df = pd.read_csv("tcc_ceds_music.csv")
 
-raw_df = pd.read_csv("tcc_ceds_music.csv")
+    corpus = [] 
+    for lyric in music_df['lyrics']:
+        corpus.append(lyric)
 
-corpus = [] 
-for lyric in raw_df['lyrics']:
-    corpus.append(lyric)
+    scores = []
+    doc_id_list = list(range(1,28373))
+    for doc_id in tqdm(range(1,28373)):
+        scores.append(_score(query, doc_id, corpus, indexes))
+    #print(scores)
+    sorted_scores = sorted(scores, reverse=True)
+    #print(sorted_scores)
+    sorted_docsID = [x for _, x in sorted(zip(scores, doc_id_list), reverse=True)]
+    #print(sorted_docsID)
 
-scores = []
-doc_id_list = list(range(1,28373))
-for doc_id in tqdm(range(1,28373)):
-    scores.append(_score(query, doc_id, corpus, indexes))
-#print(scores)
-sorted_scores = sorted(scores, reverse=True)
-#print(sorted_scores)
-sorted_docsID = [x for _, x in sorted(zip(scores, doc_id_list), reverse=True)]
-#print(sorted_docsID)
-
-# limit no of recommendations
-n = 5
-recommended_docs = sorted_docsID[0:n]
-print(recommended_docs)
+    # get recommended song infos
+    recommended_song_infos = []
+    for docID in sorted_docsID[0:n]:
+        recommended_song_infos.append(music_df['artist_name'][docID-1] + " - " + music_df['track_name'][docID-1] + ", " + str(music_df['release_date'][docID-1]) + " " + music_df['genre'][docID-1])
+    return recommended_song_infos
     
 
 
