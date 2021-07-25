92eb20b8a0b445c4ee389b94a82892ade9728678
zenn <zennpng@gmail.com>
2021-07-25 15:29:13 +0800

    added "not" handler, cleaned file

--- query_preprocessing.py
+++ query_preprocessing.py
@@ -1,20 +1,36 @@
 import nltk
 from itertools import chain
+from more_itertools import locate
 from nltk.corpus import wordnet as wn
 from numpy import exp2
 
 def process_query(query):
+
+    # define stopwords
     stopwords = nltk.corpus.stopwords.words("english")
-    stopwords.extend(['please', 'want', 'like', 'music', 'thanks', 'need', 'require'])  # extend stopwords manually
+    stopwords.extend(['please', 'want', 'like', 'music', 'thanks', 'need', 'require', 'feeling'])  # extend stopwords manually
+    
+    # tokenize query
     query_tokens = nltk.word_tokenize(query)
+
+    # replace "not something" with the antonym of "something"
+    not_loc = list(locate(query_tokens, lambda x: x == 'not'))
+    targets = [query_tokens[i+1] for i in not_loc]
+    for index in sorted(not_loc, reverse=True):
+        del query_tokens[index+1]
+        del query_tokens[index]
+    expansion = PyDictionary(targets).getAntonyms()
+    for target in expansion:
+        query_tokens.append(target[next(iter(target))][0])
+
+    # remove stopwords
     query_tokens = [w for w in query_tokens if w.lower() not in stopwords]
+
     return query_tokens
 
-# test_query = "i need some workout energy now please thanks"
-# print(process_query(test_query))
-# ['workout', 'energy']
 
-def query_expansion_old(query_list):
+
+def query_expansion_old(query_list):  ### DEPRECATED (DOES NOT LOOK VERY ACCURATE)
     expanded_query = []
     for word in query_list:
         synonyms = wn.synsets(word)
@@ -23,21 +39,25 @@ def query_expansion_old(query_list):
         expanded_query += expansion
     return(expanded_query)
 
-#print(query_expansion_old(["gym","tired","motivation","exertion"]))
 
 
 # NEW QUERY EXPANSION (more conservative)
 import pickle
 from PyDictionary import PyDictionary
 
+# get global lyrics pool of words
 with open("lyrics_vocab.txt", "rb") as fp: 
     lyrics_vocab = pickle.load(fp)
 
 def query_expansion(query_list):
+
+    # get word synonyms
     expansion = PyDictionary(query_list).getSynonyms()
     for word_expanded in expansion:
         query_list += word_expanded[next(iter(word_expanded))]
+
+    # remove phrases and words which are not part of the global lyrics pool of words
     query_list = [ x for x in query_list if " " not in x and x in lyrics_vocab ]
+    
     return query_list
         
-print(query_expansion(["energy", "workout", "feelings"]))
