50c87ccbfe632dad5e6b8a265ec50d025f720dd0
zenn <zennpng@gmail.com>
2021-08-01 21:52:54 +0800

    clean

--- VSM1_1.py
+++ VSM1_1.py
@@ -21,7 +21,6 @@ import numpy as np
 
 #query: list of words after query expansion
 #method: to measure similairity
-query = ["miss", "family"] 
 
 
 #VSM
@@ -39,47 +38,47 @@ def type_of_vsm(wv, query, vsm_type=1, method="cosine", n=5):
 
     def vsm(query, method, n, wv):
         
-            # import gensim.downloader as api
-            # wv = api.load('word2vec-google-news-300')
-            word_vec_dict = {}
-            query_vec = 0
-            for i in range(len(query)):
-                if query[i] in wv:
-                    
-                    word_vec_dict[query[i]] = wv[query[i]]
-                    query_vec = query_vec + word_vec_dict[query[i]]
-    
-            
-            doc_id_list = list(range(1,28373))
-            
-            
-            
-            if method == "cosine":
-                sim_list = []
-                for key, value in tqdm(file.items()):
-                    cos_sim = 1 - sp.spatial.distance.cosine(value, query_vec)
-                    sim_list.append(cos_sim)
-                 #sort from highest value to lowest  
-                sorted_ID = [x for _, x in sorted(zip(sim_list, doc_id_list), reverse=True)]
+        # import gensim.downloader as api
+        # wv = api.load('word2vec-google-news-300')
+        word_vec_dict = {}
+        query_vec = 0
+        for i in range(len(query)):
+            if query[i] in wv:
                 
-            if method == "euclid":
-                e_dist_list = []
-                for key, value in tqdm(file.items()):
-                    dist = np.linalg.norm(value-query_vec)
-                    e_dist_list.append(dist)
-                #sort from lowest value to highest
-                sorted_ID = [x for _, x in sorted(zip(e_dist_list, doc_id_list))]
+                word_vec_dict[query[i]] = wv[query[i]]
+                query_vec = query_vec + word_vec_dict[query[i]]
+
+        
+        doc_id_list = list(range(1,28373))
+        
+        
+        
+        if method == "cosine":
+            sim_list = []
+            for key, value in tqdm(file.items()):
+                cos_sim = 1 - sp.spatial.distance.cosine(value, query_vec)
+                sim_list.append(cos_sim)
+                #sort from highest value to lowest  
+            sorted_ID = [x for _, x in sorted(zip(sim_list, doc_id_list), reverse=True)]
             
-            if method == "dotprod":
-                prod_list =[]
-                for key, value in tqdm(file.items()):
-                    prod = np.inner(value, query_vec)
-                    prod_list.append(prod)
-                    
-                #sort from highest to lowest value
-                sorted_ID = [x for _, x in sorted(zip(prod_list, doc_id_list), reverse=True)]
+        if method == "euclid":
+            e_dist_list = []
+            for key, value in tqdm(file.items()):
+                dist = np.linalg.norm(value-query_vec)
+                e_dist_list.append(dist)
+            #sort from lowest value to highest
+            sorted_ID = [x for _, x in sorted(zip(e_dist_list, doc_id_list))]
+        
+        if method == "dotprod":
+            prod_list =[]
+            for key, value in tqdm(file.items()):
+                prod = np.inner(value, query_vec)
+                prod_list.append(prod)
                 
-            return(sorted_ID[:n], prod_list[:n])   
+            #sort from highest to lowest value
+            sorted_ID = [x for _, x in sorted(zip(prod_list, doc_id_list), reverse=True)]
+            
+        return(sorted_ID[:n], prod_list[:n])   
     
     sorted_ID_final, prod_list_final = vsm(query, method, n, wv)            
     recommended_song_infos = []
@@ -89,6 +88,7 @@ def type_of_vsm(wv, query, vsm_type=1, method="cosine", n=5):
         
     return(recommended_song_infos,sorted_ID_final[:n], prod_list_final)
             
+# old version
 def type_of_vsm_old(query, vsm_type=1, method="cosine", n=5):
     music_df = pd.read_csv("tcc_ceds_music.csv")
     
@@ -102,48 +102,48 @@ def type_of_vsm_old(query, vsm_type=1, method="cosine", n=5):
 
     def vsm(query, method, n):
         
-            import gensim.downloader as api
-            wv = api.load('word2vec-google-news-300')
-            word_vec_dict = {}
-            query_vec = 0
-            for i in range(len(query)):
-                if query[i] in wv:
-                    
-                    word_vec_dict[query[i]] = wv[query[i]]
-                    query_vec = query_vec + word_vec_dict[query[i]]
-    
-            
-            doc_id_list = list(range(1,28373))
-            
-            
-            
-            if method == "cosine":
-                sim_list = []
-                for key, value in tqdm(file.items()):
-                    cos_sim = 1 - sp.spatial.distance.cosine(value, query_vec)
-                    sim_list.append(cos_sim)
-                 #sort from highest value to lowest  
-                sorted_ID = [x for _, x in sorted(zip(sim_list, doc_id_list), reverse=True)]
+        import gensim.downloader as api
+        wv = api.load('word2vec-google-news-300')
+        word_vec_dict = {}
+        query_vec = 0
+        for i in range(len(query)):
+            if query[i] in wv:
                 
-            if method == "euclid":
-                e_dist_list = []
-                for key, value in tqdm(file.items()):
-                    dist = np.linalg.norm(value-query_vec)
-                    e_dist_list.append(dist)
-                #sort from lowest value to highest
-                sorted_ID = [x for _, x in sorted(zip(e_dist_list, doc_id_list))]
+                word_vec_dict[query[i]] = wv[query[i]]
+                query_vec = query_vec + word_vec_dict[query[i]]
+
+        
+        doc_id_list = list(range(1,28373))
+        
+        
+        
+        if method == "cosine":
+            sim_list = []
+            for key, value in tqdm(file.items()):
+                cos_sim = 1 - sp.spatial.distance.cosine(value, query_vec)
+                sim_list.append(cos_sim)
+                #sort from highest value to lowest  
+            sorted_ID = [x for _, x in sorted(zip(sim_list, doc_id_list), reverse=True)]
             
-            if method == "dotprod":
-                prod_list =[]
-                for key, value in tqdm(file.items()):
-                    prod = np.inner(value, query_vec)
-                    prod_list.append(prod)
-                    
-                #sort from highest to lowest value
-                sorted_ID = [x for _, x in sorted(zip(prod_list, doc_id_list), reverse=True)]
+        if method == "euclid":
+            e_dist_list = []
+            for key, value in tqdm(file.items()):
+                dist = np.linalg.norm(value-query_vec)
+                e_dist_list.append(dist)
+            #sort from lowest value to highest
+            sorted_ID = [x for _, x in sorted(zip(e_dist_list, doc_id_list))]
+        
+        if method == "dotprod":
+            prod_list =[]
+            for key, value in tqdm(file.items()):
+                prod = np.inner(value, query_vec)
+                prod_list.append(prod)
                 
-            return(sorted_ID[:n], prod_list[:n])   
-    
+            #sort from highest to lowest value
+            sorted_ID = [x for _, x in sorted(zip(prod_list, doc_id_list), reverse=True)]
+            
+        return(sorted_ID[:n], prod_list[:n])   
+
     sorted_ID_final, prod_list_final = vsm(query, method, n)            
     recommended_song_infos = []
     for docID in sorted_ID_final[0:n]:
@@ -151,7 +151,7 @@ def type_of_vsm_old(query, vsm_type=1, method="cosine", n=5):
                 
         
     return(recommended_song_infos,sorted_ID_final[:n], prod_list_final)        
-     
+    
         
     
     
--- engine.py
+++ engine.py
@@ -2,13 +2,14 @@ import query_preprocessing
 import VSM1_1
 import bm25_full
 import LangModel
+import gensim.downloader as api
+
 
 def generate_recommendations(query, number):
     tokenQuery = query_preprocessing.process_query(query)
     expandedQuery = query_preprocessing.query_expansion(tokenQuery)
-    vsm1_1_recommendations, sortID, prodlist = VSM1_1.type_of_vsm(expandedQuery, method = "dotprod", vsm_type = 1, n=number) 
-    selected_docsID_bm25, bm25_recommendations = bm25_full.bm25(expandedQuery, n=number)
-    selected_docsID_Lang, langmodel_recommendations = LangModel.langmodel(expandedQuery, n=number)
+    wv = api.load('word2vec-google-news-300')
+    vsm1_1_recommendations, sortID, prodlist = VSM1_1.type_of_vsm(wv, expandedQuery, method = "dotprod", vsm_type=1, n=number) 
     return vsm1_1_recommendations
 
-#print(generate_recommendations("lovely day great energy", 5))
+print(generate_recommendations("happy and joyful", 4))
